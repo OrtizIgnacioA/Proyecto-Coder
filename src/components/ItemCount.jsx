e8cec2c7fbf08aa0f7dae3860d75ef434f26d30e
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2021-02-02 21:19:49 -0300

    Contador con boton
    Se agrega contador utilizando useState

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,24 @@
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+export const ItemCount = ({ stock, count, onSubtract, onAdd }) => {
+  return (
+    <>
+      <ButtonGroup aria-label='Basic example'>
+        <Button onClick={onSubtract}>-</Button>
+        <Button>{count}</Button>
+        {count < stock ? (
+          <Button
+            onClick={() => {
+              onAdd(stock);
+            }}
+          >
+            +
+          </Button>
+        ) : (
+          <Button variant='warning'>Solo hasta 6 Productos</Button>
+        )}
+      </ButtonGroup>
+      <Button>Comprar</Button>
+    </>
+  );
+};
--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+import { ItemCount } from '../components/ItemCount';
+
 const ItemListContainer = ({ greeting }) => {
+  const [count, setCount] = useState(1);
+
+  const onAdd = (stock) => {
+    console.log(stock);
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  const onSubtract = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      console.log('Valor no valido');
+    }
+  };
+
   return (
     <>
       <a className='h1 row justify-content-center'>{greeting}</a>
+      <ItemCount
+        stock={6}
+        count={count}
+        onSubtract={onSubtract}
+        onAdd={onAdd}
+      />
     </>
   );
 };
